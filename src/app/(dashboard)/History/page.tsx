be6e60d5230077dd40374e9468c90fb076214ae7
mark valdez <jhonmarkprudentevaldezcoder@gmail.com>
2023-11-19 13:37:41 +0800

    da

--- src/app/(dashboard)/History/page.tsx
+++ src/app/(dashboard)/History/page.tsx
@@ -83,27 +83,27 @@ export default function History() {
   }, [userToken]);
 
   return (
-    <div className="container mx-auto md:mt-32 mt-24">
-      <div className="flex shadow-md my-10">
-        <div className="w-3/4 bg-white px-10 py-10">
+    <div className="md:p-12 mx-auto md:mt-32 mt-24">
+      <div className="flex flex-col md:shadow-md   lg:flex-row">
+        <div className="w-full lg:w-3/4 bg-white md:p-5 lg:px-10 lg:py-10">
           <div className="flex justify-between border-b pb-8">
-            <h1 className="font-semibold text-2xl">Shopping History </h1>
+            <h1 className="font-semibold text-2xl p-2">Shopping History</h1>
             <h2 className="font-semibold text-2xl">
               {" "}
               {totalProducts !== null ? (
                 <p>{totalProducts}</p>
               ) : (
-                <p>Loading total...</p>
+                <p>---------</p>
               )}
             </h2>
           </div>
-          <div className="flex mt-10 mb-5">
-            <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">
-              Product Details{" "}
+          <div className="flex mt-10 mb-5 p-2">
+            <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5  ">
+              Product Details
               {checkOutID !== null ? (
-                <p>{checkOutID}</p>
+                <p className="hidden md:flex">{checkOutID}</p>
               ) : (
-                <p>Loading total...</p>
+                <p></p>
               )}
             </h3>
             <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 ">
@@ -117,7 +117,7 @@ export default function History() {
             </h3>
           </div>
 
-          <div>
+          <div className="p-4">
             {cartProducts.map((product) => (
               <div
                 className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5"
@@ -156,9 +156,22 @@ export default function History() {
               </div>
             ))}
           </div>
+
+          <Link
+            href="/"
+            className="flex font-semibold text-indigo-600 text-sm mt-10 p-6"
+          >
+            <svg
+              className="fill-current mr-2 text-indigo-600 w-4"
+              viewBox="0 0 448 512"
+            >
+              <path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" />
+            </svg>
+            Continue Shopping
+          </Link>
         </div>
 
-        <div id="summary" className="w-1/4 px-8 py-10">
+        <div id="summary" className="md:w-full lg:w-1/4 px-8 py-10">
           <h1 className="font-semibold text-2xl border-b pb-8">
             Order Summary
           </h1>
@@ -167,14 +180,14 @@ export default function History() {
               {totalProducts !== null ? (
                 <p> Items {totalProducts}</p>
               ) : (
-                <p>Loading total...</p>
+                <p>---------...</p>
               )}
             </span>
             <span className="font-semibold text-sm">
               {totalPrice !== null ? (
                 <p>₱{parseInt(totalPrice).toFixed(2)}</p>
               ) : (
-                <p>Loading total...</p>
+                <p>---------...</p>
               )}
             </span>
           </div>
@@ -191,10 +204,11 @@ export default function History() {
             <div className="flex font-semibold justify-between py-6 text-sm uppercase">
               <span>Total cost</span>
               <span>
+                {" "}
                 {totalPrice !== null ? (
                   <p>₱{(parseInt(totalPrice) + 50).toFixed(2)}</p>
                 ) : (
-                  <p>Loading total...</p>
+                  <p>---------...</p>
                 )}
               </span>
             </div>
